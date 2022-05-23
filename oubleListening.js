23d0ae418477f04a9848036ef57c82f83bf78517
npc <82518037+wbsnpc@users.noreply.github.com>
2022-05-23 10:26:19 +0800

    Update oubleListening.js

--- oubleListening.js
+++ oubleListening.js
@@ -1,1 +1,22 @@
 
+export default {
+    async fetch(request, env) {
+      const SingleDay = 'tr53.herokuapp.com'
+      const DoubleDay = 'rt53.herokuapp.com'
+      let host = ''
+      let nd = new Date();
+      if (nd.getDate()%2) {
+          host = SingleDay
+      } else {
+          host = DoubleDay
+      }
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        url.hostname=host;
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      // Otherwise, serve the static assets.
+      return env.ASSETS.fetch(request);
+    }
+  };
