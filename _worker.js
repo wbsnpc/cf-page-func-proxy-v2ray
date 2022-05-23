2ff978e36fb6e955d8277bf203950a8fe10b622d
npc <82518037+wbsnpc@users.noreply.github.com>
2022-05-23 10:06:29 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,18 +1,8 @@
-
 export default {
     async fetch(request, env) {
-      const SingleDay = 'tr53.herokuapp.com'
-      const DoubleDay = 'rt53.herokuapp.com'
-      let host = ''
-      let nd = new Date();
-      if (nd.getDate()%2) {
-          host = SingleDay
-      } else {
-          host = DoubleDay
-      }
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname=host;
+        url.hostname="testv2app.wbsnpc.workers.dev";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
