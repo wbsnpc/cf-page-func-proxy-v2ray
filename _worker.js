48746818cca9ff5bdfa4e2cf9b3c3627d00d145d
npc <82518037+wbsnpc@users.noreply.github.com>
2022-06-21 15:31:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="testv2app.wbsnpc.workers.dev";
+        url.hostname="163.172.67.56:17263";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
