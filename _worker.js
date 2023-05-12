89c9df6b90572b708e9d7a53f4c179e7a685d546
npc <82518037+wbsnpc@users.noreply.github.com>
2023-05-12 16:38:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="https://reader-replit.wbsnpc.repl.co";
+        url.hostname="http://reader-replit.wbsnpc.repl.co";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
